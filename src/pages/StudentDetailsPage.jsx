fab4afe385c35bf6b0523893aa86dd8b0c1b0c94
HannaFarha <johnnyfarha1@gmail.com>
2023-12-13 17:18:57 +0100

    almost done

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,6 +1,6 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
-
+import {Link} from 'react-router-dom';
 function StudentDetailsPage() {
 
   // Find the current student profile by id.
@@ -67,9 +67,10 @@ function StudentDetailsPage() {
 
 
             {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            <Link to='/student' ><button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
               Back
-            </button>
+            </button></Link>
+            </div>
 
           </>
         )}
